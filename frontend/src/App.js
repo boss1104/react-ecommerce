1a672da9fa300ff880f7bbc6eaffdae6607067a7
boss1104 <alphaBoss1104@gmail.com>
2022-10-03 22:52:06 +0300

    1.Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">online-shop</a>
+      </header>
+      <main>list</main>
+    </div>
+  );
+}
+
+export default App;
